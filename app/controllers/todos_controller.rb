963a0ce9c6c2c61b95f0515d3944ff346cf0785e
carinah <carina@generalassemb.ly>
2014-06-07 12:41:42 -0700

    initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,23 @@
+class TodosController < ApplicationController
+	def index
+		@todos = Todo.all
+	end 
+
+	def show 
+		@todo = Todo.find(params[:id])
+	end 
+
+	def new 
+		@todo = Todo.new
+	end 
+
+  def create 
+    @todo = Todo.create(todo_params)
+  end 
+
+  private 
+    def todo_params
+      params.require(:todo).permit(:title, :description)
+    end 
+    # return the title and description from new.html.erb
+end
