5020d209ce02e64bf8a7193dbb55702ee99cc264
carinah <cyingho@gmail.com>
2014-07-09 09:11:12 -0700

    Fixing spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'todos#index'
 
-get '/todos', to: "todos#index"
+  get '/todos', to: "todos#index"
 
-get '/todos/:id', to: "todos#show"
+  get '/todos/:id', to: "todos#show"
 end
